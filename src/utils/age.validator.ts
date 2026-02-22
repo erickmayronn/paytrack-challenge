2fe908ab28a12fd2c182cf0984c51edb05052b96
idealPad <erickmayronn@gmail.com>
2026-02-22 11:21:36 -0300

    feat: fisrt commit plus age validator

---
+++ src/utils/age.validator.ts
@@ -0,0 +1,19 @@
+export default function validateLegalAge(dob: string): boolean {
+  const [year, month, day] = dob.split('-').map(Number);
+  const dateOfBirth = new Date(year, month - 1, day);
+
+  if (isNaN(dateOfBirth.getTime())) return false;
+
+  const today = new Date();
+  const legalAge = 18;
+
+  const eighteenYearsAgo = new Date(
+    today.getFullYear() - legalAge,
+    today.getMonth(),
+    today.getDate(),
+  );
+
+  console.log(eighteenYearsAgo);
+
+  return dateOfBirth <= eighteenYearsAgo;
+}
